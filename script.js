d8b60e69fb6dd93775e075424105b6f3047d4be0
Santiago Palacios <santipalacios2002@gmail.com>
2021-04-30 17:21:37 -0400

    added some of the data into the 5 day forecast via function

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var cityArrayWithLonLat = [];
 var cityArrayTags = [];
-var cityJson = [];
 var cityAndState = [];
 var submitBtn = document.querySelector('#searchBtn');
 var cityEl = document.querySelector('#tags');
-var searchedCity 
+var dateEl = document.querySelector('.date');
+var weatherJson = [];
 
 
 //gets city json file of American cities
-$.getJSON('./city.list.json', function(data) {         
+$.getJSON('./city.list.json', function (data) {
   for (var i = 0; i < data.length; i++) {
     cityArrayWithLonLat.push({
-      'name': data[i].name, 
-      'state': data[i].state, 
+      'name': data[i].name,
+      'state': data[i].state,
       'lon': data[i].coord.lon,
-      'lat': data[i].coord.lat 
+      'lat': data[i].coord.lat
     })
     cityArrayTags.push(`${data[i].name}, ${data[i].state}`)
   }
@@ -22,14 +22,14 @@ $.getJSON('./city.list.json', function(data) {
 
 
 //jquery autocomplete
-$( function() {
-    $( "#tags" ).autocomplete({
-      source: cityArrayTags
-    });
-  } );
+$(function () {
+  $("#tags").autocomplete({
+    source: cityArrayTags
+  });
+});
 
 
-$(submitBtn).on('click', function() {
+$(submitBtn).on('click', function () {
   var citySearched = $('#tags').val(); //extracts the value from search
   cityAndState = citySearched.split(",")
   console.log(cityAndState)
@@ -41,7 +41,6 @@ $(submitBtn).on('click', function() {
   }
 
   var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLonLat[0]}&lon=${cityLonLat[1]}&appid=0f6d8bac92fd58296cc45805a6e84234&units=imperial&exclude=minutely,hourly`;
-  console.log(weatherUrl)
 
   $.ajax({
     url: weatherUrl,
@@ -50,9 +49,32 @@ $(submitBtn).on('click', function() {
     .then(function (response) { // runs if no error happens
       console.log('Ajax Reponse \n-------------');
       console.log(response);
+      weatherJson = response;   //might get rid of it later, but this makes the json glabal and accessible
+      weatherCurrent(response);
+      weatherForecast(response);
     })
     .catch(function (error) { // runs if an error happens
       console.log('error:', error);
     });
 
 })
+
+//building the date text
+for (let index = 0; index < 5; index++) {
+  $('.forecast').children().children('.date').eq(index).text(moment().add(index + 1, 'days').format('l'));
+}
+
+
+function weatherCurrent(apiJson) {
+  console.log('hello')
+  for (let index = 0; index < 5; index++) {
+    $('.icon').eq(index).attr('src', `https://openweathermap.org/img/wn/${apiJson.daily[index + 1].weather[0].icon}@2x.png`);
+    $('.temp').eq(index).text(`${apiJson.daily[index + 1].temp.day}`)
+
+  }
+}
+
+function weatherForecast(apiJson) {
+  console.log('hello2')
+  $()
+}

3f8b8cefd44c9ec2fc6bf014dcff2ab829f53011
Santiago Palacios <santipalacios2002@gmail.com>
2021-05-03 22:53:52 -0400

    added some logic to the search and fixed some bugs. added uv index bg color

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var cityEl = document.querySelector('#tags');
 var dateEl = document.querySelector('.date');
 var weatherJson = [];
 var cityStorage = [];
+searchBlock();
 
 //gets city json file of American cities
 $.getJSON('./city.list.json', function (data) {
@@ -20,8 +21,7 @@ $.getJSON('./city.list.json', function (data) {
   }
 });
 
-
-//jquery autocomplete
+//jquery autocomplete widget
 $(function () {
   $("#tags").autocomplete({
     source: cityArrayTags
@@ -29,12 +29,15 @@ $(function () {
 });
 
 
-$(submitBtn).on('click', function () {
+
+
+$('#submitBtn').on('click', function () {
   var citySearched = $('#tags').val(); //extracts the value from search
-  cityAndState = citySearched.split(",")
+  console.log('this is the city searched', citySearched)
+  cityAndState = citySearched.split(", ")
   console.log(cityAndState)
   for (var i = 0; i < cityArrayWithLonLat.length; i++) {
-    if (cityAndState[0] === cityArrayWithLonLat[i].name && cityAndState[1].trim() === cityArrayWithLonLat[i].state) {
+    if (cityAndState[0] === cityArrayWithLonLat[i].name && cityAndState[1] === cityArrayWithLonLat[i].state) {
       var cityLonLat = [cityArrayWithLonLat[i].lat, cityArrayWithLonLat[i].lon]
       console.log(cityLonLat)
     }
@@ -56,9 +59,10 @@ $(submitBtn).on('click', function () {
     .catch(function (error) { // runs if an error happens
       console.log('error:', error);
     });
-    saveSearch($('#tags').val())
-
+    saveSearch($('#tags').val());
+    $('.search-block').prepend(`<button class="btn btn-secondary col mb-2 searchBtn">${citySearched}</button>`)
 })
+
 
 //building the date text for forecast
 for (let index = 0; index < 5; index++) {
@@ -69,7 +73,7 @@ for (let index = 0; index < 5; index++) {
 function weatherForecast(apiJson) {
   console.log('hello')
   for (let index = 0; index < 6; index++) {
-    $('.icon').eq(index).attr('src', `https://openweathermap.org/img/wn/${apiJson.daily[index].weather[0].icon}.png`);
+    $('.icon').eq(index).attr('src', `https://openweathermap.org/img/wn/${apiJson.daily[index].weather[0].icon}@2x.png`);
     $('.icon').eq(index).attr('alt', `weather icon`);
     $('.temp').eq(index).text(`${Math.round(apiJson.daily[index + 1].temp.max)}/${Math.round(apiJson.daily[index + 1].temp.min)} ℉`)
     $('.wind').eq(index).text(`${apiJson.daily[index + 1].wind_speed} MPH`)
@@ -85,14 +89,23 @@ function weatherCurrent(apiJson) {
   $('.tempCurrent').text(`${Math.round(apiJson.current.temp)} ℉`)
   $('.windCurrent').text(`${Math.round(apiJson.current.wind_speed)} MPH`)
   $('.humidityCurrent').text(`${Math.round(apiJson.current.humidity)} %`)
-  $('.uvIndexCurrent').text(`${Math.round(apiJson.current.uvi)}`)
+  $('.uvIndexCurrent').text(`${apiJson.current.uvi}`)
+  if (apiJson.current.uvi < 3) {
+    $('.uvIndexCurrent').addClass('bg-success text-white rounded px-2 py-1 fw-bold')
+  } else if (apiJson.current.uvi < 6) {
+    $('.uvIndexCurrent').addClass('bg-warning text-white rounded px-2 py-1 fw-bold')
+  } else {
+    $('.uvIndexCurrent').addClass('bg-danger text-white rounded px-2 py-1 fw-bold')
+  }
+
 }
 
 
 //use unshift to add new element at the beggining of the array
-//use pop to remove last eelement of the array
+//use pop to remove last element of the array
 var searchIndex = 7;
-function saveSearch(city) {
+
+function saveSearch(city) {                                         //saves searches to local storage when invoked
   var citySearch = JSON.parse(localStorage.getItem('search'));
   if (citySearch === null) {
     cityStorage.unshift(city)
@@ -102,22 +115,10 @@ function saveSearch(city) {
     cityStorage.unshift(city)
     localStorage.setItem('search', JSON.stringify(cityStorage)) //save it to local storage
   }
-  
-  searchIndex++;
 }
 
-function searchBlock() {
-  var citySearch = JSON.parse(localStorage.getItem('search'));
-  if (citySearch === null) {
-    return
-  }
-  for (let index = 0; index < citySearch.length; index++) {    
-    $('#search').append(`<button class="btn btn-secondary col mb-2 searchBtn">${citySearch[index]}</button>`) 
-  }
-}
-
-searchBlock()
-
+//ask in class!!!!!
+//=================
 $('.searchBtn').on('click', function (event) {
   console.log(event.target.innerText)
   var citySearched = event.target.innerText
@@ -129,21 +130,33 @@ $('.searchBtn').on('click', function (event) {
       console.log(cityLonLat)
     }
   }
-
+  
   var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLonLat[0]}&lon=${cityLonLat[1]}&appid=0f6d8bac92fd58296cc45805a6e84234&units=imperial&exclude=minutely,hourly`;
-
+  
   $.ajax({
     url: weatherUrl,
     method: 'GET',
   })
-    .then(function (response) { // runs if no error happens
-      console.log('Ajax Reponse \n-------------');
-      console.log(response);
-      weatherJson = response;   //might get rid of it later, but this makes the json glabal and accessible
-      weatherCurrent(response);
-      weatherForecast(response);
-    })
-    .catch(function (error) { // runs if an error happens
-      console.log('error:', error);
-    });
+  .then(function (response) { // runs if no error happens
+    console.log('Ajax Reponse \n-------------');
+    console.log(response);
+    weatherJson = response;   //might get rid of it later, but this makes the json glabal and accessible
+    weatherCurrent(response);
+    weatherForecast(response);
+  })
+  .catch(function (error) { // runs if an error happens
+    console.log('error:', error);
+  });
 })
+
+
+function searchBlock() {
+  var citySearch = JSON.parse(localStorage.getItem('search'));
+  if (citySearch === null) {
+    return
+  }
+  // $('.search-block').empty() //empties the search block first before building the block
+  for (let index = 0; index < citySearch.length; index++) {    
+    $('.search-block').append(`<button class="btn btn-secondary col mb-2 searchBtn">${citySearch[index]}</button>`) 
+  }
+}
